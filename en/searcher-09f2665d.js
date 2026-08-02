be2efb3e0082179c86066512125fec6da9a83625
k4m7v2pz <k4m7v2pz@users.noreply.github.com>
2026-08-02 14:08:51 +0000

    deploy: 33df8de9f5c219820026ff776cd3e9e89085e6ac

--- en/searcher-09f2665d.js
+++ en/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b154ec3a.js',
+                path_to_root + 'searchindex-74e41637.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
