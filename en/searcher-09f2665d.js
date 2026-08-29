245092d9c4cccf5843407096395ae731155c9338
k4m7v2pz <k4m7v2pz@users.noreply.github.com>
2026-08-29 06:45:23 +0000

    deploy: 2fe2966e683def891d3e4621b0fc6f7f18bd4171

--- en/searcher-09f2665d.js
+++ en/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-0e00918b.js',
+                path_to_root + 'searchindex-78c2476f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
--- zh/searcher-09f2665d.js
+++ zh/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-de4a49d1.js',
+                path_to_root + 'searchindex-27f4d3dc.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
